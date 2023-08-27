ea99bf331ff277f5ff2d782eb9a629a3894a5236
anand9703122459 <anoty946@gmail.com>
2023-08-27 10:10:31 +0530

    practise

---
+++ program1/aw.py
@@ -0,0 +1,9 @@
+print("welcome to the program of loops")
+a=5
+for i in range(10):
+    print("loop continues")
+if(a>10):
+          print("the number is higher")
+else:
+          print("the number i lower")
+print("the loops was exit") 
